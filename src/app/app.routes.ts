3874e92236c7607b22a46adcaae74d607c110623
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-01 15:07:41 -0400

    Removed extra export ensuring default export is used

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [
+const routes: Routes = [
   {
     path: '',
     title: 'Home Page',
