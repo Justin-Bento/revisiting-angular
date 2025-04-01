4c806d62ebacd12ccc59c03e83a63a83d3a8141e
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-01 14:40:34 -0400

    Generated projects component

---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  imports: [],
+  template: `
+    <p>
+      projects works!
+    </p>
+  `,
+  styleUrl: './projects.component.scss'
+})
+export class ProjectsComponent {
+
+}
