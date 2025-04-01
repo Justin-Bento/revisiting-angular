63ba0bf27c64578604dce1a8fc6d14ccb2602714
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-01 15:12:58 -0400

    Updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,35 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {
     path: '',
     title: 'Home Page',
     component: HomeComponent,
+  },
+  {
+    path: '/about',
+    title: 'About Page',
+    component: AboutComponent,
+  },
+  {
+    path: '/projects',
+    title: 'Proejcts Page',
+    component: ProjectsComponent,
+  },
+  {
+    path: '/blog',
+    title: 'Blog  Page',
+    component: BlogComponent,
+  },
+  {
+    path: '/contact',
+    title: 'Contact Page',
+    component: ContactComponent,
   },
 ];
 export default routes;
